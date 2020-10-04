1617c49fee3e73fcf2ff4a66d596f906648dbd2d
algani14 <alghani837@gmail.com>
2020-10-04 11:33:26 +0700

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Resawan
+                    Hotel
                 </a>
                 <a class="navbar-brand" href=" {{ route('typekamar.index') }} ">
                         type kamar
